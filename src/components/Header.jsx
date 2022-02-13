a85dc431379bbdcd10951149231d24f6ca918480
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-13 23:22:22 +0600

    Create product component, improve header & footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,56 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container-fluid">
-				<span className="navbar-brand mb-0 h1">RDX Shopping</span>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container">
+				<Link className="navbar-brand" to="/">
+					RDX Shopping
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div
+					className="collapse navbar-collapse justify-content-end"
+					id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<NavLink
+								className="nav-link mx-2"
+								aria-current="page"
+								to="/">
+								Home
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link mx-2" to="/about">
+								About
+							</NavLink>
+						</li>
+					</ul>
+				</div>
 			</div>
+
+			<style jsx="true">{`
+				.navbar-nav li a {
+					font-family: "Open Sans", sans-serif;
+					color: #ffc200 !important;
+					font-weight: 500;
+					font-size: 18px;
+					text-transform: uppercase;
+				}
+				.navbar-nav li a:hover,
+				.navbar-nav li a.active {
+					color: #ff7600 !important;
+				}
+			`}</style>
 		</nav>
 	);
 };
---
+++ src/components/Product/Products.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import data from "../../data.json";
+import Product from "./Product";
+
+const Products = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		setProducts(data.products);
+	}, []);
+	console.log(products);
+
+	return (
+		<div className="row row-cols-1 row-cols-lg-3 row-cols-sm-2 g-4">
+			{products &&
+				products.map((product) => (
+					<Product product={product} key={product._id} />
+				))}
+		</div>
+	);
+};
+
+export default Products;
