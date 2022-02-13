f6825522ad50500c00caa653f11aa87ebc5cdcc4
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-13 20:16:08 +0600

    Setup basic layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<div className="container-fluid">
+				<span className="navbar-brand mb-0 h1">RDX Shopping</span>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
