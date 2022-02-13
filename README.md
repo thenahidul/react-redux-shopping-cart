93082611aa83a356bf5c1d6528723f7a588c6535
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-13 20:05:54 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Redux Shopping Cart Project
+
+# Step 1: Create React App
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className="container">
+			<header className="App-header">Another Branch</header>
+		</div>
+	);
+};
+
+export default App;
