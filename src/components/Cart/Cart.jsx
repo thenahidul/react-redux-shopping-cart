de7ec976d2d842d19f53faa3918b2c4f40cceec7
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-16 18:54:50 +0600

    Add animation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,12 +3,11 @@ import { MdDeleteForever } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
 import { formatCurrency } from "../../functions";
 import { clearCart, removeFromCart } from "../../utils/store/cartSlice";
+import { Fade } from "react-awesome-reveal";
 
 const Cart = () => {
-	// const [cart, setCart] = useState([]);
 	const [total, setTotal] = useState(0);
 	const cart = useSelector((state) => state.cart.list);
-	const a = useSelector((state) => state.cart);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -26,56 +25,60 @@ const Cart = () => {
 				Cart
 				<small> ({cart.length})</small>
 			</h3>
-			<ul className="list-unstyled d-grid gap-3 mb-0">
-				{cart.length ? (
-					cart.map((cart) => (
-						<li
-							className="d-flex position-relative text-right"
-							key={cart._id}>
-							<img
-								className="thumb img-thumbnail"
-								src={cart.image}
-								alt={cart.title}
-							/>
-							<div className="px-3">
-								<h6 className="mt-0 mb-1 card-title cart-title">
-									{cart.title}
-								</h6>
-								<div className="text-secondary cart-meta">
-									<span className="amount">
-										{formatCurrency(cart.price)}
-									</span>
-									<span>x</span>
-									<span className="quantity">{cart.qty}</span>
-									<div>
-										<span className="size">
-											Size: {cart.size},
+			{cart.length ? (
+				<Fade cascade direction="left" triggerOnce={true}>
+					<ul className="list-unstyled d-grid gap-3 mb-0">
+						{cart.map((cart) => (
+							<li
+								className="d-flex position-relative text-right"
+								key={cart._id}>
+								<img
+									className="thumb img-thumbnail"
+									src={cart.image}
+									alt={cart.title}
+								/>
+								<div className="px-3">
+									<h6 className="mt-0 mb-1 card-title cart-title">
+										{cart.title}
+									</h6>
+									<div className="text-secondary cart-meta">
+										<span className="amount">
+											{formatCurrency(cart.price)}
 										</span>
-										<span className="color">
-											Color: {cart.color}
+										<span>x</span>
+										<span className="quantity">
+											{cart.qty}
+										</span>
+										<div>
+											<span className="size">
+												Size: {cart.size},
+											</span>
+											<span className="color">
+												Color: {cart.color}
+											</span>
+										</div>
+										<span
+											className="position-absolute remove"
+											onClick={() =>
+												dispatch(
+													removeFromCart({
+														_id: cart._id
+													})
+												)
+											}>
+											<MdDeleteForever />
 										</span>
 									</div>
-									<span
-										className="position-absolute remove"
-										onClick={() =>
-											dispatch(
-												removeFromCart({
-													_id: cart._id
-												})
-											)
-										}>
-										<MdDeleteForever />
-									</span>
 								</div>
-							</div>
-						</li>
-					))
-				) : (
-					<p className="text-center text-muted">
-						No items added in the cart
-					</p>
-				)}
-			</ul>
+							</li>
+						))}
+					</ul>
+				</Fade>
+			) : (
+				<p className="text-center text-muted">
+					No items added in the cart
+				</p>
+			)}
 			{cart.length > 0 && (
 				<div className="border-top pt-3 mt-4">
 					<h4 className="text-end mt-2">
