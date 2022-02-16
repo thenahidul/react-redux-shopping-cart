69305c7f91b9ca37be43ecfbe45114f4ff0b570a
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-16 18:17:18 +0600

    Create addToCart component

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+		<div className="container min-vh-100 py-5">
+			<div className="row">
+				<div className="col">
+					<h1>Cart</h1>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Ullam libero at sed nulla omnis nostrum suscipit nam harum
+					doloribus eum tempore, nemo molestias, dicta possimus
+					eveniet illum voluptas. Amet, pariatur.
+				</div>
+			</div>
+		</div>
+  );
+}
+
+export default Cart
