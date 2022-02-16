7419646ecef21c32df99d55c3147b40c6b97f8ff
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-02-17 00:29:36 +0600

    Change

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,15 @@
 export function formatCurrency(num) {
 	return "$" + Number(num.toFixed(2)).toLocaleString() + " ";
 }
+
+export function setCartLocalStorage(value) {
+	localStorage.setItem("cart", JSON.stringify(value));
+}
+
+export function getCartLocalStorage() {
+	return JSON.parse(localStorage.getItem("cart"));
+}
+
+export function clearCartLocalStorage() {
+	localStorage.clear("cart");
+}
