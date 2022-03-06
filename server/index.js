9f75b9636cd8090a8a53c0f5feb539bdb254e43e
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-03-06 12:55:38 +0600

    port

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
