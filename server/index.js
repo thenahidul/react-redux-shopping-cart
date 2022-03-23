726512d2fe96105957a11e129eae511d8673cbaa
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-03-23 13:02:51 +0600

    chnages

--- server/index.js
+++ server/index.js
@@ -22,10 +22,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../build")));
+	app.use(express.static(path.resolve(__dirname, "../build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../build/index.html"));
+		res.sendFile(path.resolve(__dirname, "../build/index.html"));
 	});
 }
 
