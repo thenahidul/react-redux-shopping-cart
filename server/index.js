530ac1a562a3427ae9f859a194681263dd298648
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-03-23 12:43:41 +0600

    changes

--- server/index.js
+++ server/index.js
@@ -21,13 +21,13 @@ app.use(cors());
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../build")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../build/index.html"));
-});
-// }
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../build/index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
